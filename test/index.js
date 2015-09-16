7dc18aa3eb2c5881a51d3928b2f6511e43c66ddc
Geoffrey Challen <challen@buffalo.edu>
2015-09-16 17:00:53 -0400

    New.

--- test/index.js
+++ test/index.js
@@ -1,18 +1,20 @@
 require('harmonize')(['harmony-generators']);
 
 var metalsmith = require('metalsmith'),
-    linkcheck = require('..'),
     fs = require('fs'),
     path = require('path'),
-    chai = require('chai');
+    chai = require('chai'),
+    linkcheck = require('..'),
+    defaults = require('../lib/defaults');
 
+console.log(defaults);
 chai.use(require('chai-fs'));
 var assert = chai.assert;
 
 function linkcheck_test(src, options, fn) {
   var once = false;
 
-  metalsmith(src).use(linkcheck.check(options)).build(function(err, files) {
+  metalsmith(src).use(linkcheck(options)).build(function(err, files) {
     if (once) return;
     once = true;
     fn(err, files);
@@ -21,24 +23,24 @@ function linkcheck_test(src, options, fn) {
 
 describe('metalsmith-linkcheck', function() {
   var src = 'test/fixtures/errors';
-  var defaults = linkcheck.defaults(path.join(src, 'src'));
+  var test_defaults = defaults(path.join(src, 'src'));
   try {
-    fs.unlinkSync(defaults.checkFile);
+    fs.unlinkSync(test_defaults.checkFile);
   } catch (err) {};
   try {
-    fs.unlinkSync(defaults.failFile);
+    fs.unlinkSync(test_defaults.failFile);
   } catch (err) {};
-  assert.notPathExists(defaults.ignoreFile);
-  assert.notPathExists(defaults.checkFile);
-  assert.notPathExists(defaults.failFile);
+  assert.notPathExists(test_defaults.ignoreFile);
+  assert.notPathExists(test_defaults.checkFile);
+  assert.notPathExists(test_defaults.failFile);
 
   it('should work and not fail without parameters', function(done){
     linkcheck_test(src, {}, function(err, files) {
       if (err) {
         return done(err);
       }
-      assert.pathExists(defaults.checkFile);
-      assert.pathExists(defaults.failFile);
+      assert.pathExists(test_defaults.checkFile);
+      assert.pathExists(test_defaults.failFile);
       return done();
     });
   });

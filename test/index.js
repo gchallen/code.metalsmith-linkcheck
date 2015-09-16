4138d09af150e415632e75fd424586e2b3943044
Geoffrey Challen <challen@buffalo.edu>
2015-09-16 14:32:44 -0400

    More tests passing.

--- test/index.js
+++ test/index.js
@@ -1,13 +1,18 @@
 require('harmonize')(['harmony-generators']);
 
-var assert = require('assert'),
-    metalsmith = require('metalsmith'),
-    linkcheck = require('..');
+var metalsmith = require('metalsmith'),
+    linkcheck = require('..'),
+    fs = require('fs'),
+    path = require('path'),
+    chai = require('chai');
+
+chai.use(require('chai-fs'));
+var assert = chai.assert;
 
 function linkcheck_test(src, options, fn) {
   var once = false;
 
-  metalsmith(src).use(linkcheck(options)).build(function(err, files) {
+  metalsmith(src).use(linkcheck.check(options)).build(function(err, files) {
     if (once) return;
     once = true;
     fn(err, files);
@@ -16,10 +21,24 @@ function linkcheck_test(src, options, fn) {
 
 describe('metalsmith-linkcheck', function() {
   var src = 'test/fixtures/errors';
+  var defaults = linkcheck.defaults(path.join(src, 'src'));
+  try {
+    fs.unlinkSync(defaults.checkFile);
+  } catch (err) {};
+  try {
+    fs.unlinkSync(defaults.failFile);
+  } catch (err) {};
+  assert.notPathExists(defaults.ignoreFile);
+  assert.notPathExists(defaults.checkFile);
+  assert.notPathExists(defaults.failFile);
 
   it('should work and not fail without parameters', function(done){
     linkcheck_test(src, {}, function(err, files) {
-      if (err) return done(err);
+      if (err) {
+        return done(err);
+      }
+      assert.pathExists(defaults.checkFile);
+      assert.pathExists(defaults.failFile);
       return done();
     });
   });

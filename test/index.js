94ec1fd7d0d9f801ecf02f5bebb02b2980c423af
Geoffrey Challen <challen@buffalo.edu>
2015-09-16 18:27:53 -0400

    Painful.

--- test/index.js
+++ test/index.js
@@ -3,27 +3,16 @@ require('harmonize')(['harmony-generators']);
 var metalsmith = require('metalsmith'),
     fs = require('fs'),
     path = require('path'),
+    _ = require('underscore'),
     chai = require('chai'),
+    jsonfile = require('jsonfile'),
     linkcheck = require('..'),
-    defaults = require('../lib/defaults');
+    linkcheckDefaults = require('../lib/linkcheckDefaults.js');
 
-console.log(defaults);
 chai.use(require('chai-fs'));
 var assert = chai.assert;
 
-function linkcheck_test(src, options, fn) {
-  var once = false;
-
-  metalsmith(src).use(linkcheck(options)).build(function(err, files) {
-    if (once) return;
-    once = true;
-    fn(err, files);
-  })
-}
-
-describe('metalsmith-linkcheck', function() {
-  var src = 'test/fixtures/errors';
-  var test_defaults = defaults(path.join(src, 'src'));
+function reset_files(test_defaults) {
   try {
     fs.unlinkSync(test_defaults.checkFile);
   } catch (err) {};
@@ -33,15 +22,67 @@ describe('metalsmith-linkcheck', function() {
   assert.notPathExists(test_defaults.ignoreFile);
   assert.notPathExists(test_defaults.checkFile);
   assert.notPathExists(test_defaults.failFile);
+}
 
-  it('should work and not fail without parameters', function(done){
-    linkcheck_test(src, {}, function(err, files) {
-      if (err) {
-        return done(err);
-      }
-      assert.pathExists(test_defaults.checkFile);
-      assert.pathExists(test_defaults.failFile);
-      return done();
-    });
+var internal_broken = [
+  "/assets/css/broken.css",
+  "/assets/img/broken.jpg",
+  "/broken.jpg",
+  "/broken.html",
+  "/www.broken.com",
+  "/assets/js/broken.js",
+];
+var external_broken = [
+  "https://www.google.com/broken.css",
+  "https://g.twimg.com/broken.png",
+  "http://www.google.com/broken.html",
+  "http://www.google.com/broken.js"
+];
+var all_broken = internal_broken.concat(external_broken);
+
+describe('metalsmith-linkcheck', function() {
+  it('should identify all broken links with the default parameters', function(done) {
+    var src = 'test/fixtures/errors';
+    var defaults = _.clone(linkcheckDefaults.defaults);
+    var test_defaults = linkcheckDefaults.processConfig(path.join(src, 'src'), defaults);
+    reset_files(test_defaults);
+
+    metalsmith(src)
+      .use(linkcheck(defaults))
+      .build(function (err) {
+        if (err) {
+          return done(err);
+        }
+        assert.pathExists(test_defaults.checkFile);
+        assert.pathExists(test_defaults.failFile);
+
+        var broken = jsonfile.readFileSync(test_defaults.failFile);
+        assert.deepEqual(broken.sort(), all_broken.sort());
+
+        done();
+      });
   });
+  
+  it('should identify only internal broken links when asked to fail', function(done) {
+    var src = 'test/fixtures/errors';
+    var defaults = _.clone(linkcheckDefaults.defaults);
+    defaults.failMissing = true;
+    var test_defaults = linkcheckDefaults.processConfig(path.join(src, 'src'), defaults);
+    reset_files(test_defaults);
+
+    metalsmith(src)
+      .use(linkcheck(defaults))
+      .build(function (err) {
+        if (!err) {
+          return done(new Error("should fail"));
+        } else {
+          assert.pathExists(test_defaults.failFile);
+
+          var broken = jsonfile.readFileSync(test_defaults.failFile);
+          assert.deepEqual(broken.sort(), internal_broken.sort());
+
+          done();
+        }
+      });
+    });
 });
